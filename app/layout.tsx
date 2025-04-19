a397d0a471ae065e4b2f7a692c5e3beb5e18437c
meglerhagen <Christer.hagen@gmail.com>
2025-04-19 19:24:55 +0200

    updated Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GeistSans } from "geist/font/sans";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased ${GeistSans.className}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
